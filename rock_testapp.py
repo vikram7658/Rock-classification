c98945cb7905d039250965bf0026ab73ca02ce06
vikram7658 <66861377+vikram7658@users.noreply.github.com>
2020-08-24 15:22:58 +0545

    Add files via upload

---
+++ rock_testapp.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Rock Class Prediction App
+This app predicts the **Rock Class type** !
+Data obtained from the [https://www.kaggle.com/achintjain/model-comparison-voting-classifier-visualisation/comments
+]""")
+
+st.sidebar.header('User Input Features')
+
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+# collects user features into dataframe
+uploaded_file = st.sidebar.file_uploader("upload your input CSV file", type=['csv'])
+
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+
+else:
+    def user_input_features():
+        D_m = st.sidebar.slider('Tunnel_diameter (m)', 2.5, 13.0, 3.0)
+        Q = st.sidebar.slider('Q ', 0.001, 1.0, 93.5)
+        H_m = st.sidebar.slider('H (m)', 52.0, 850.0, 200.0)
+        k = st.sidebar.slider('k', 0.0, 5324.0, 4207.0)
+        E = st.sidebar.slider('E', 0.0, 36.73, 20.0)
+        data = {'D_m': D,
+                'Q': Q,
+                'H_m': H,
+                'k': k,
+                'E': E}
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+
+    input_df = user_input_features()
+
+#combining user features with entire data set
+rock_data = pd.read_csv('rockclass.csv')
+rock = rock_data.drop(columns=['Class'])
+df = pd.concat([input_df, rock], axis=0)
+
+#displays the iser input features
+st.subheader("user Input fetures")
+
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('awaiting csv file to be uploaded, currently using examplefile')
+    st.write(df)
+
+load_rf = pickle.load(open('Random_forest.pkl', 'rb'))
+
+#apply model to make prediction
+prediction = load_rf.predict(df)
+prediction_prob = load_rf.predict_proba(df)
+
+st.subheader('Prediction')
+rock_class = np.array(['sever', 'mild', 'minor'])
+st.write(rock_class[prediction])
+st.subheader('prediction probability')
+st.write(prediction_prob)
